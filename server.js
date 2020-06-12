088c07acbcf8c96b704cce017a7403aee44177fa
Alon Zuman <alonzuman7@gmail.com>
2020-06-12 13:38:20 +0300

    more setup

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const tours = require('./routes/api/tours')
-const users = require('./routes/api/users')
+const tours = require('./routes/api/tours');
+const users = require('./routes/api/users');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tours', tours);
 app.use('/api/v1/users', users);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
